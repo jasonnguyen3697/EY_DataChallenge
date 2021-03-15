163192a1504dc5bce361cd843b158ce5b189f0c4
jasonnguyen3697 <jasonnguyen3697@gmail.com>
2021-03-15 11:42:02 +1100

    Move calculation of treatment later than ordering to Transformation.py

--- PopulationAnalysis.py
+++ PopulationAnalysis.py
@@ -10,27 +10,39 @@
 """
 import pandas as pd
 import scipy.stats as stats
+from sklearn import linear_model
+import statsmodels.api as sm
 
 # read dataset and timeline
 transformedDataset = pd.read_excel("output.xlsx", sheet_name="Dataset_ED_transformed")
 presentationTimeline = pd.read_excel("output.xlsx", sheet_name="Presentation Timeline")
 
-# calculate population treated after their expected ordering
-dateTimeTreatedLaterThanOrdering = list(presentationTimeline.loc[presentationTimeline["Actual Ranking"] > presentationTimeline["Expected Ranking"]]["Datetime"].unique())
-treatedLaterThanOrdering = presentationTimeline.loc[presentationTimeline["Actual Ranking"] > presentationTimeline["Expected Ranking"]][["MRN", "Presentation Visit Number"]].drop_duplicates(subset=["MRN", "Presentation Visit Number"], keep="first")
-
-# add flag to transformed dataset
-transformedDataset = transformedDataset.merge(treatedLaterThanOrdering, how="left", on=["MRN", "Presentation Visit Number"], indicator=True)
-mask = transformedDataset["_merge"] == "both"
-transformedDataset.loc[mask, "TreatedLaterThanOrdering"] = 1
-transformedDataset.loc[~mask, "TreatedLaterThanOrdering"] = 0
-transformedDataset.drop(labels=["_merge"], axis="columns", inplace=True)
-
 # test assumption that LateSeenByDr is a normal distr
 normTest = stats.shapiro(transformedDataset["LateSeenByDr"])
 
 # t-test for TreatedLaterThanOrdering
-res = ttest_ind(transformedDataset.loc[(transformedDataset["TreatedLaterThanOrdering"]==0) & ~(transformedDataset["LateSeenByDr"].isna())]["LateSeenByDr"], transformedDataset.loc[(transformedDataset["TreatedLaterThanOrdering"]==1) & ~(transformedDataset["LateSeenByDr"].isna())]["LateSeenByDr"], equal_var=False)
+res = stats.ttest_ind(transformedDataset.loc[(transformedDataset["TreatedLaterThanOrdering"]==0) & ~(transformedDataset["LateSeenByDr"].isna())]["LateSeenByDr"], transformedDataset.loc[(transformedDataset["TreatedLaterThanOrdering"]==1) & ~(transformedDataset["LateSeenByDr"].isna())]["LateSeenByDr"], equal_var=False)
 
 # graph density for TreatedLaterThanOrdering and LateSeenByDr
 ax = transformedDataset[["TreatedLaterThanOrdering", "LateSeenByDr"]].pivot(columns="TreatedLaterThanOrdering", values="LateSeenByDr").plot.density(alpha=0.5, figsize=(15, 12))
+
+# multi-variate linear regression
+# calculate accepatable range
+mean = transformedDataset["LateSeenByDr"].mean()
+threeStd = transformedDataset["LateSeenByDr"].std() * 3
+lowerBound = mean - threeStd
+upperBound = mean + threeStd
+
+# using scikit-learn
+reg = linear_model.LinearRegression()
+reg.fit(transformedDataset.loc[~transformedDataset["LateSeenByDr"].isna()][["Triage 1 count", "Triage 2 count", "Triage 3 count", "Triage 4 count", "Triage 5 count"]], transformedDataset.loc[~transformedDataset["LateSeenByDr"].isna()]["LateSeenByDr"])
+
+# calculate r2 of model
+r2 = reg.score(transformedDataset.loc[~transformedDataset["LateSeenByDr"].isna()][["TotalPatientsInEDAtArrival", "Triage 1 count", "Triage 2 count", "Triage 3 count", "Triage 4 count", "Triage 5 count"]], transformedDataset.loc[~transformedDataset["LateSeenByDr"].isna()]["LateSeenByDr"])
+
+# using statsmodel
+est = sm.OLS(transformedDataset.loc[(~transformedDataset["LateSeenByDr"].isna()) & (transformedDataset["LateSeenByDr"] >= lowerBound) & (transformedDataset["LateSeenByDr"] <= upperBound)]["LateSeenByDr"], transformedDataset.loc[(~transformedDataset["LateSeenByDr"].isna()) & (transformedDataset["LateSeenByDr"] >= lowerBound) & (transformedDataset["LateSeenByDr"] <= upperBound)][["Triage 1 count", "Triage 2 count", "Triage 3 count", "Triage 4 count", "Triage 5 count"]])
+
+est = est.fit()
+
+print(est.summary())
